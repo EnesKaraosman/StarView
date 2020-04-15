abb2f6754dee18bb37627160ecbc099cf286d380
Enes Karaosman <enes.karaosman@etiya.com>
2020-04-15 06:13:45 +0300

    [MOD] Star Shape improved.

--- Sources/StarView/StarShape.swift
+++ Sources/StarView/StarShape.swift
@@ -9,23 +9,39 @@ import SwiftUI
 
 public struct StarShape: Shape {
     
+    private func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
+        return CGPoint(x: radius * cos(angle) + offset.x, y: radius * sin(angle) + offset.y)
+    }
+    
     public func path(in rect: CGRect) -> Path {
-        
-        let quarter: CGFloat = rect.midX * 0.5 // 1/4
-        let bit: CGFloat = quarter * 0.5  // 1/8
-
         var path = Path()
-        
-        path.move(to: .init(x: rect.midX, y: rect.minY)) // Top Center
-        path.addLine(to: .init(x: rect.midX + bit, y: quarter))
-        path.addLine(to: .init(x: rect.maxX, y: quarter))
-        path.addLine(to: .init(x: rect.midX + quarter, y: 2 * quarter))
-        path.addLine(to: .init(x: rect.maxX - bit, y: rect.maxY)) // Bottom Right
-        path.addLine(to: .init(x: rect.midX, y: rect.midY + bit)) // Bottom Center
-        path.addLine(to: .init(x: rect.minX + bit, y: rect.maxY)) // Bottom Left
-        path.addLine(to: .init(x: rect.midX - quarter, y: 2 * quarter))
-        path.addLine(to: .init(x: rect.minX, y: quarter))
-        path.addLine(to: .init(x: rect.midX - bit, y: quarter))
+
+        let starExtrusion: CGFloat = 20.0
+
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+
+        let pointsOnStar = 5
+
+        var angle: CGFloat = -.pi / 2
+        let angleIncrement = CGFloat(.pi * 2.0 / Double(pointsOnStar))
+        let radius = rect.midX
+
+        for i in 1...pointsOnStar {
+            
+            let point = self.pointFrom(angle: angle, radius: radius, offset: center)
+            let nextPoint = self.pointFrom(angle: angle + angleIncrement, radius: radius, offset: center)
+            let midPoint = self.pointFrom(angle: angle + angleIncrement / 2.0, radius: starExtrusion, offset: center)
+
+            if i == 1 { // First point
+                path.move(to: point)
+            }
+
+            path.addLine(to: midPoint)
+            path.addLine(to: nextPoint)
+
+            angle += angleIncrement
+        }
+
         path.closeSubpath()
 
         return path
